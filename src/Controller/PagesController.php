e042a4c3429421f9ff4657f52e9d5a701cafdc3b
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2021-11-13 16:37:18 +0100

    first commit

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('pages/index.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+
+    public function about(): Response
+    {
+        return $this->render('pages/about.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+}
